3adcf3c6b5a3e93ee300e625996c648c867903e4
BrendenSeifried <brendenseifried@gmail.com>
2022-06-06 14:54:55 -0700

    starting testing as website works!

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,6 +3,8 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
+const { zodaics } = require('../data/zodiacs');
+
 describe('Zodiac-API routes', () => {
   beforeEach(() => {
     return setup(pool);
