e9727264d1bdb4dc54d77333049a40bd735d948e
BrendenSeifried <brendenseifried@gmail.com>
2022-06-06 15:08:37 -0700

    both tests pass

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,11 +3,35 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-const { zodaics } = require('../data/zodiacs');
+const { zodiacs } = require('../data/zodiacs');
 
 describe('Zodiac-API routes', () => {
   beforeEach(() => {
     return setup(pool);
+  });
+
+  it('list of zodiacs tested here', async () => {
+    const resp = await request(app).get('/zodiacs');
+    const expected = zodiacs.map((item) => {
+      return {
+        id: item.id,
+        name: item.name,
+        dates: item.dates,
+        symbol: item.symbol,
+      };
+    });
+    expect(resp.body).toEqual(expected);
+  });
+
+  it('checking single zodiac test', async () => {
+    const resp = await request(app).get('/zodiacs/5');
+    const testingFive = {
+      id: '5',
+      name: 'gemini',
+      dates: 'May 21 - Jun 20',
+      symbol: 'Twins',
+    };
+    expect(resp.body).toEqual(testingFive);
   });
 
   afterAll(() => {
